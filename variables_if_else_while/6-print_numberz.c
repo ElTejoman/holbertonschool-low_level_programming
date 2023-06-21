803bb6d871a3dad2f08410d740b782029e5a33f3
ElTejoman <aespinoza.ascue@gmail.com>
2023-06-21 16:42:01 -0700

    Adding spaces required by betty

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
- * main - Program tht prints all single digits numbers of base 10. only using putchar.
+ * main - Program tht prints all single digits numbers of base 10..
  *
  * Return: 0 (success)
  */
 
-int main (void)
+int main(void)
 {
 	int num;
 
 	for (num = 0; num < 10; num++)
 		putchar ((num % 10) + '0');
 
-	putchar ('\n');
+	putchar('\n');
 
 	return (0);
 }
